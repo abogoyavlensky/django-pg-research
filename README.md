8fce451d7e362e3b7337cfa6a5bfb4ffb12bc3b5
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2015-06-30 13:44:17 +0300

    Add queries by overlap.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # django-pg-research
 Sample app for attempts dealing with new functions in djanog for PostgreSQL
+
+Installation
+---------
+
+    $ pip install requirements.pip
+    $ python manage.py generate
+    $ python manage.py runserver
+
+
+*Then you could open browser: http://localhost:8000/ and check some query results*
--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+import random
+
 from django.views.generic import TemplateView
 
+from app.models import Tag
+from app.models import Post
 
 class Home(TemplateView):
     template_name = 'app/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        tags_ids = Tag.objects.all().values_list('id', flat=True)
+        rand = random.sample(tags_ids, 5)
+        context['rand'] = rand
+        context['overlap'] = Post.objects.filter(tags__overlap=rand)
+        return context
 
 home = Home.as_view()
 
